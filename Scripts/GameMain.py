dccf1c719c8523002c9836c300660d00529ed480
MatzeHa <m.hainmueller@gmail.com>
2020-03-15 11:38:55 +0100

    Add files via upload

--- Scripts/GameMain.py
+++ Scripts/GameMain.py
@@ -1,9 +1,12 @@
 import pygame
 import os
 
-from Scripts.Entity import Entity
-from Scripts.PauseMenu import PauseMenu
-from Scripts.Controls import controls_game, controls_pause
+
+from Scripts.GameModes.PauseMenu import PauseMenu
+from Scripts.Level.LVLMain import LVLMain
+from Scripts.Util.GameLoop import game_loop
+from Scripts.GameModes.CreateChar import CreateChar
+from Scripts.Util.Functions import get_coords
 
 pygame.init()
 
@@ -12,74 +15,44 @@ if __name__ == "__main__":
         def __init__(self):
             self.win = pygame.Surface((0, 0))
             self.dirtyrects = []
-            self.win_w = 800
-            self.win_h = 800
+            self.win_w = 1900
+            self.win_h = 1068
             self.win_size = (self.win_w, self.win_h)
             self.bgcol = (0, 0, 255)
+            self.pause_menu = 0
+            self.wall_w = 150
+            self.wall_h = 150
+            self.cell_size = 64
+            self.coord = {"w": get_coords(self.win_w, self.wall_w, self.cell_size),
+                          "h": get_coords(self.win_h, self.wall_h, self.cell_size)}
 
         def update_bg(self, _win):
-            self.bgcol = pause_menue.options["bgcol"]
+            self.bgcol = setup.pause_menue.options["bgcol"]
             _win.fill(self.bgcol)
             self.win = _win.copy()
 
     setup = Setup()
-    os.environ['SDL_VIDEO_WINDOW_POS'] = "0, 33"
+    os.environ['SDL_VIDEO_WINDOW_POS'] = "0, 0"
     win = pygame.display.set_mode((setup.win_w, setup.win_h))
     pygame.display.set_caption("Matzes Kneipenspiel", "MK")
     setup.win = win.copy()
-    clock = pygame.time.Clock()
-    run = True
 
+    '''
     entity = Entity()
     opponents = []
     for i in range(0, 5):
         opponents.append(Entity())
-    pause_menue = PauseMenu(win, setup)
+    '''
 
+    setup.pause_menu = PauseMenu(win, setup)
     pygame.mouse.set_visible(False)
-    while run:
-        # EINGABE
-        if pause_menue.active:
-            run = controls_pause(pause_menue)
 
-            if pause_menue.end_pause:
-                setup.dirtyrects.append(pause_menue.dirtyrect)
-                setup.update_bg(win)
-                pause_menue.reset_pause_menue()
-            else:
-                setup.dirtyrects = [pause_menue.blitten()]
-
-
-        else:
-            run = controls_game(entity, pause_menue, setup)
-            for i in opponents:
-                i.random_walk()
-
-            win.blit(win, (0, 0))
-            pygame.display.update()
-
-            # Überblitten
-            entity.overblit(win, setup)
-            for i in opponents:
-                i.overblit(win, setup)
-            setup.dirtyrects = []
+    create_char = CreateChar(setup)
+    create_char.start_creation(win)
 
 
 
-            # Berechnungen
+    lvl_main = LVLMain(win, setup)
+    win, g = lvl_main.init_draw(win, create_char)
 
-            setup.dirtyrects.append(entity.movement())
-            for i in opponents:
-                setup.dirtyrects.append(i.movement())
-
-            # Blitten
-            entity.blitten(win)
-            for i in opponents:
-                i.blitten(win)
-
-        if not pause_menue.quit:
-            clock.tick(30)
-            pygame.display.update(setup.dirtyrects)
-        else:
-            pygame.quit()
-            run = False
+    game_loop(setup, g)
